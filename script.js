b30838ff56346b727448de54945c35a785ea4d2e
Dinis <example@gmail.com>
2020-10-31 20:12:09 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm('Вы сегодня отдыхаете?');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', '');
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Какой ваш возраст?', '');
+// answers[2] = prompt('Когда вы готовы приступить к работе?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+// console.log(`https://someurl/${category}/5`);
+
+// const user = 'Dinis';
+// alert(`Привет, ${user}`);
+
+// console.log(4 + ' 6');
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+// console.log(incr++);
+// console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
+
+console.log(2 + 2 * 2 !== '6');
+
+// const isChecked = true,
+//       isClose = false;
+// console.log(isChecked || isClose);
