c3f3e14c14e8f5eda15292e2653797cde9ce3e93
Dinis <example@gmail.com>
2020-11-14 12:23:51 +0500

    Прошли Массивы и псевдомассивы

--- script.js
+++ script.js
@@ -1,53 +1,32 @@
 "use strict";
 
-const options = {
-  name: 'test',
-  width: 1024,
-  height: 1024,
-  colors: {
-    border: 'black',
-    background: 'red'
-  },
-  makeTest: function() {
-    console.log('It is test');
-  }
-};
-options.makeTest();
+const arr = [9, 12, 10, 36, 31, 8];
+arr.sort(compareNum);
+console.log(arr);
 
-const {border, background} = options.colors;
+function compareNum(a, b) {
+  return a - b;
+}
 
-console.log(border, background);
-// console.log(Object.keys(options).length);
+// arr[99] = 0;
+// console.log(arr);
+// arr.forEach(function(item, i, arr) {
+//   console.log(` ${i}: ${item} внутри массива ${arr}`);
+// });
+// arr.pop();
+// arr.push(10);
 
-// console.log(options['colors'] ['border']);
+// console.log(arr);
 
-// delete options.name;
-
-// console.log(options);
-
-// let counter = 0;
-
-// for (let key in options) {
-//   if (typeof(options[key]) == 'object') {
-//     for (let i in options[key]) {
-//       console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-      
-//     }
-//   } else {
-//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//     counter++;
-//   }
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
 // }
-// console.log(`Количество свойств в этом объекте: ${counter}`);
 
-// const girl = {
-//   name: 'Карина',
-//   height: 160,
-//   color: {
-//     eyes: 'grey',
-//     hair: 'milirovny'
-//   },
-//   vote: 'tender'
-// };
+// for (let value of arr) {
+//   console.log(value);
+// }
 
-// console.log(Object.keys(girl).length);
+// const str = prompt('', '', '');
+// const products = str.split(', ');
+// products.sort();
+// console.log(products.join('; '));
