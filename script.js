bcb47344a93a823d8e1a684ed79e002645e9e2f5
Dinis <example@gmail.com>
2020-11-15 20:30:07 +0500

    Передача по ссылке или по значению, Spread оператор

--- script.js
+++ script.js
@@ -1,32 +1,102 @@
 "use strict";
 
-const arr = [9, 12, 10, 36, 31, 8];
-arr.sort(compareNum);
-console.log(arr);
+let a = 5,
+    b = a;
 
-function compareNum(a, b) {
-  return a - b;
+b = b + 5;
+console.log(b);
+console.log(a);
+
+const obj = {
+  a: 5,
+  b: 1
+};
+
+// const copy = obj; //Cсылка на исходный объект
+
+// copy.a = 10;
+// console.log(copy);
+// console.log(obj);
+
+function copy (mainObj) { 
+  let objCopy = {};
+
+  let key;
+  for (key in mainObj) {
+    objCopy[key] = mainObj[key];
+  }
+  return objCopy;
 }
 
-// arr[99] = 0;
-// console.log(arr);
-// arr.forEach(function(item, i, arr) {
-//   console.log(` ${i}: ${item} внутри массива ${arr}`);
-// });
-// arr.pop();
-// arr.push(10);
+const numbers = {
+  a: 2,
+  b: 5,
+  c: {
+    x: 7,
+    y: 4
+  }
+};
 
-// console.log(arr);
+// const newNumbers = copy(numbers);
 
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i]);
-// }
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
 
-// for (let value of arr) {
-//   console.log(value);
-// }
+// console.log(newNumbers);
+// console.log(numbers);
 
-// const str = prompt('', '', '');
-// const products = str.split(', ');
-// products.sort();
-// console.log(products.join('; '));
+const add = {
+  d: 17,
+  e: 20
+};
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'z';
+
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(video);
+console.log(blogs);
+console.log(internet);
+
+function log(a, b, c) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ['a', 'b'];
+
+const newAaray = [...array];
+
+console.log(array);
+console.log(newAaray);
+
+const qwer = {
+  1: 'Alex',
+  2: 'Bob',
+  3: 'Maria'
+};
+
+const names = {...qwer};
+
+console.log(qwer);
+console.log(names);
