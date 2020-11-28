a00952ac49c4140d720ab82c72cbe327e15be2f5
Dinis <example@gmail.com>
2020-11-28 13:38:50 +0500

    Отлавливали ошибки при помощи breakpoints

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
 "use strict";
 
-let str = 'some';
-// let strObj = new String(str);
+function hello() {
+  console.log('Hello, world!');
+}
+hello();
 
-// console.log(typeof(str));
-// console.log(typeof(strObj));
-
-console.dir([1,2,3]);
-
-const soldier = {
-  health: 400,
-  armor: 100,
-  sayHello: function() {
-    console.log('Hello!');
-  }
-};
-
-const john = Object.create(soldier);
-
-// const john = {
-//   health: 150
-// };
-
-// john.__proto__ = soldier;
-
-// Object.setPrototypeOf(john, soldier); //Так мы делаем если динамически нам нужно получить свойства
-
-// // console.log(john.armor);
-john.sayHello();
+function hi() {
+  console.log('Say, hi');
+}
+hi();
