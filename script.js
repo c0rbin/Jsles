a90f78102e03955a6949961de25029682703a155
Dinis <example@gmail.com>
2020-12-01 13:07:06 +0500

    Получили элементы со страницы

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
 "use strict";
 
-// let x = 5;
-// alert(x++);
+const box = document.getElementById('box');
+console.log(box);
 
-// [ ] + false - null + true 
-// console.log([] + false - null + true); //Ответ NaN
+const btns = document.getElementsByTagName('button');
+console.log(btns[1]);
 
-// let y = 1;
-// let x = y = 2;
-// alert(x);
+const circle = document.getElementsByClassName('circle');
+console.log(circle);
 
-// console.log([] + 1 + 2); //Ответ 12
-// alert('1'[0]); //Ответ 1
+const hearts = document.querySelectorAll('.heart');
+console.log(hearts);
 
-// console.log(2 && 1 && null && 0 && undefined); //Ответ null
+hearts.forEach(item=> {
+  console.log(item);
+});
 
-// *******------ Оператор И останавливается на лжи------ *****************
-// *******------ Оператор ИЛИ останавливается на правде------ *****************
-
-// console.log(!!( 1 && 2 ) === (1 && 2)); //Ответ не равны
-            // 3
-// alert( null || 2 && 3 || 4 ); //Ответ 3
-
-// const a = [1, 2, 3];
-// const b = [1, 2, 3];
-// console.log(a == b);
-
-// alert(+Infinity);
-
-// console.log('Ёжик' > 'яблоко');
-
-// console.log(0 || "" || 2 || undefined || true || falsе);
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
