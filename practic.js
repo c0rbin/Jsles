ddf803313b7664c032741358c7a2edc9755c8d4e
Dinis <example@gmail.com>
2021-02-06 12:40:20 +0500

    Обновляем знания, повторяем заново

--- practic.js
+++ practic.js
@@ -1,6 +1,16 @@
 'use sctrict';
+//Практика.Часть 1
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+  while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+}
+start();
 
 const perconalMovieDB = {
   count: numberOfFilms,
@@ -10,15 +20,48 @@ const perconalMovieDB = {
   privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильсов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильсов', ''),
-      d = prompt('На сколько оцените его?', '');
+// Практика.Часть 2
 
-perconalMovieDB.movies[a] = b;
-perconalMovieDB.movies[c] = d;
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильсов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    
+    if (a!= null && b != null && a != '' && b !='' && a.length < 50) {
+      perconalMovieDB.movies[a] = b;
+      console.log('done');
+    } else {
+      console.log('error!');
+      i--;
+    }
+  }
+}
+// rememberMyFilms();
 
-console.log(perconalMovieDB);
+function detectPersonalLevel() {
+  if (perconalMovieDB.count < 10) {
+    console.log('Просмотрено слишком мало фильмов');
+  } else if (perconalMovieDB.count >= 10 && perconalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+  } else if (perconalMovieDB.count >= 30) {
+    console.log('Вы киноман!');
+  } else {
+    console.log('Произошла ошибка!');
+  }
+}
+// detectPersonalLevel();
 
+function showMyDB() {
+  if (perconalMovieDB.privat != true) {
+    console.log(perconalMovieDB);
+  }
+}
+showMyDB();
 
+function writeYourGenres () {
+  for (let i = 1; i <= 3; i++) {
+    perconalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+  } 
+}
+writeYourGenres();
 
--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 "use strict";
 
-const box = document.getElementById('box');
-console.log(box);
+let num = 20;
 
-const btns = document.getElementsByTagName('button');
-console.log(btns[1]);
 
-const circle = document.getElementsByClassName('circle');
-console.log(circle);
+function firstMessageShow(text) {
+  console.log(text);
+  
+  console.log(num);
+}
+firstMessageShow('Здравствуй Мир!');
+console.log(num);
 
-const hearts = document.querySelectorAll('.heart');
-console.log(hearts);
+// function calc(a, b) {
+//   return (a + b);
+// }
 
-hearts.forEach(item=> {
-  console.log(item);
-});
+// console.log(calc(4,3));
+// console.log(calc(5,5));
+// console.log(calc(15,15));
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+function ret() {
+  let num = 50;
+  return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+  console.log('Hello!');
+};
+logger();
+
+const calc = (a, b) => {
+  console.log('1');
+  return a + b;
+};
+console.log(calc(4,5));
