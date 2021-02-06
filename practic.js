5629780f9f8de1db0f43059c949966c1e51fbbb9
Dinis <example@gmail.com>
2021-02-06 15:34:22 +0500

    ПРактика ч3. (Повторение) Используем функции

--- practic.js
+++ practic.js
@@ -36,7 +36,7 @@ function rememberMyFilms() {
     }
   }
 }
-// rememberMyFilms();
+rememberMyFilms();
 
 function detectPersonalLevel() {
   if (perconalMovieDB.count < 10) {
@@ -49,7 +49,7 @@ function detectPersonalLevel() {
     console.log('Произошла ошибка!');
   }
 }
-// detectPersonalLevel();
+detectPersonalLevel();
 
 function showMyDB() {
   if (perconalMovieDB.privat != true) {
