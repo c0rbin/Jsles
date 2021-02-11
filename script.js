62f861d7cf15d4d89927a320808654bdbeca489f
Dinis <example@gmail.com>
2021-02-11 21:12:06 +0500

    Основы ООП повторение

--- script.js
+++ script.js
@@ -1,98 +1,29 @@
 "use strict";
 
-// const obj = {
-//   a: 5,
-//   b: 1
+let str = 'some';
+let strObj = new String(str); //Устаревший метод создания строки
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// console.dir([1, 2, 3]);
+
+const soldier = {
+  health: 400,
+  armor: 100,
+  sayHello: function() {
+    console.log('Hello!');
+  }
+};
+
+const john = Object.create(soldier);
+
+// const john = {
+//   health: 100
 // };
-// const copy = obj; //Ссылка на объект
-// copy.a = 10;
-// console.log(copy);
-// console.log(obj);
 
-function copy(mainObj) {
-  let objCopy = {};
+// Object.setPrototypeOf(john, soldier);
 
-  let key;
-  for (key in mainObj) {
-    objCopy[key] = mainObj[key];
-  }
-
-  return objCopy;
-}
-
-const numbers = {
-  a: 2,
-  b: 5,
-  c: {
-    x: 7,
-    y: 4
-  }
-};
-
-const newNumbers = copy(numbers);
-
-newNumbers.a = 41;
-newNumbers.c.x = 14;
-// console.log(numbers);
-// console.log(newNumbers);
-
-const add = {
-  d: 17,
-  e: 20
-};
-
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-console.log(add);
-console.log(clone);
-
-const oldArray = ['a', 'b', 'c'];
-
-const newArray = oldArray.slice();
-newArray[1] = 9;
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs =   ['wordpress', 'livejournal', 'blogger'],
-      internet = [...video, ...blogs, 'vk', 'fb'];
-
-console.log(internet);
-
-function log(a, b, c) {
-  console.log(a);
-  console.log(b);
-  console.log(c);
-}
-
-const num = [2, 5, 7]; 
-
-log(...num);
-
-const array = ['a', 'b'];
-
-const newArr = [...array];
-console.log(newArr);
-
-const q = {
-  one: 1,
-  two: 2
-};
-
-const newObjes = {...q};
-
-console.log(q);
-console.log(newObjes);
-
-const bond = {
-  sif: 'halk'
-};
-
-const fik = {
-  hex: 'mili'
-};
-
-const game = Object.assign(bond, fik);
-
-console.log(game);
+// john.__proto__ = soldier;
+// console.log(john, john.armor);
+john.sayHello();
