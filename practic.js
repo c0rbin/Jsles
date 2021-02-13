bee1aff3881de617c08dfe0ce4a61a8e837fdd20
Dinis <example@gmail.com>
2021-02-13 13:52:50 +0500

    Практика ч4 Повтор

--- practic.js
+++ practic.js
@@ -1,67 +1,71 @@
 'use sctrict';
-//Практика.Часть 1
-
-let numberOfFilms;
-
-function start() {
-  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-  while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-  }
-}
-start();
 
 const perconalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
-  privat: false
+  privat: false,
+  start: function() {
+    perconalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  
+    while(perconalMovieDB.count == '' || perconalMovieDB.count == null || isNaN(perconalMovieDB.count)) {
+      perconalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+  },
+  rememberMyFilms: function() {
+    for (let i = 0; i < 2; i++) {
+      const a = prompt('Один из последних просмотренных фильсов?', ''),
+            b = prompt('На сколько оцените его?', '');
+      
+      if (a!= null && b != null && a != '' && b !='' && a.length < 50) {
+        perconalMovieDB.movies[a] = b;
+        console.log('done');
+      } else {
+        console.log('error!');
+        i--;
+      }
+    }
+  },
+  detectPersonalLevel: function() {
+    if (perconalMovieDB.count < 10) {
+      console.log('Просмотрено слишком мало фильмов');
+    } else if (perconalMovieDB.count >= 10 && perconalMovieDB.count < 30) {
+      console.log('Вы классический зритель');
+    } else if (perconalMovieDB.count >= 30) {
+      console.log('Вы киноман!');
+    } else {
+      console.log('Произошла ошибка!');
+    }
+  },
+  showMyDB: function() {
+    if (perconalMovieDB.privat != true) {
+      console.log(perconalMovieDB);
+    }
+  },
+  toggleVisibleDB: function() {
+    if (perconalMovieDB.privat) {
+      perconalMovieDB.privat = false;
+    } else {
+      perconalMovieDB.privat = true;
+    }
+  },
+  writeYourGenres:  function() {
+    for (let i = 1; i <= 3; i++) {
+      let genre = prompt(`Ваш любимый жанр под номером ${i}`).toLowerCase();
+
+      if (genre == '' || genre == null) {
+        console.log('Вы ввели некорректные данные или не ввели данные');
+        i--;
+      }else {
+        perconalMovieDB.genres[i - 1] = genre;
+        perconalMovieDB.genres.sort();
+      } 
+    } 
+    perconalMovieDB.genres.forEach((item, i) => {
+      console.log(`Любимый жанр ${i + 1} - это ${item}`);
+    });
+  }
 };
 
-// Практика.Часть 2
-
-function rememberMyFilms() {
-  for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильсов?', ''),
-          b = prompt('На сколько оцените его?', '');
-    
-    if (a!= null && b != null && a != '' && b !='' && a.length < 50) {
-      perconalMovieDB.movies[a] = b;
-      console.log('done');
-    } else {
-      console.log('error!');
-      i--;
-    }
-  }
-}
-rememberMyFilms();
-
-function detectPersonalLevel() {
-  if (perconalMovieDB.count < 10) {
-    console.log('Просмотрено слишком мало фильмов');
-  } else if (perconalMovieDB.count >= 10 && perconalMovieDB.count < 30) {
-    console.log('Вы классический зритель');
-  } else if (perconalMovieDB.count >= 30) {
-    console.log('Вы киноман!');
-  } else {
-    console.log('Произошла ошибка!');
-  }
-}
-detectPersonalLevel();
-
-function showMyDB() {
-  if (perconalMovieDB.privat != true) {
-    console.log(perconalMovieDB);
-  }
-}
-showMyDB();
-
-function writeYourGenres () {
-  for (let i = 1; i <= 3; i++) {
-    perconalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-  } 
-}
-writeYourGenres();
 
--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 "use strict";
 
-let str = 'some';
-let strObj = new String(str); //Устаревший метод создания строки
+// let str = 'some';
+// let strObj = new String(str); //Устаревший метод создания строки
 
 // console.log(typeof(str));
 // console.log(typeof(strObj));
 
 // console.dir([1, 2, 3]);
 
-const soldier = {
-  health: 400,
-  armor: 100,
-  sayHello: function() {
-    console.log('Hello!');
-  }
-};
+// const soldier = {
+//   health: 400,
+//   armor: 100,
+//   sayHello: function() {
+//     console.log('Hello!');
+//   }
+// };
 
-const john = Object.create(soldier);
+// const john = Object.create(soldier);
 
 // const john = {
 //   health: 100
@@ -26,4 +26,4 @@ const john = Object.create(soldier);
 
 // john.__proto__ = soldier;
 // console.log(john, john.armor);
-john.sayHello();
+// john.sayHello();
