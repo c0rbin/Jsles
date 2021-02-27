edf814e0d8332bb16fe4bcb38e2f1b1cc767fbb2
Dinis <example@gmail.com>
2021-02-27 18:30:51 +0500

    Практика. Задание на отработку действий со страницей

--- practic.js
+++ practic.js
@@ -1,71 +1,36 @@
 'use sctrict';
 
-const perconalMovieDB = {
-  count: 0,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-  start: function() {
-    perconalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-  
-    while(perconalMovieDB.count == '' || perconalMovieDB.count == null || isNaN(perconalMovieDB.count)) {
-      perconalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-  },
-  rememberMyFilms: function() {
-    for (let i = 0; i < 2; i++) {
-      const a = prompt('Один из последних просмотренных фильсов?', ''),
-            b = prompt('На сколько оцените его?', '');
-      
-      if (a!= null && b != null && a != '' && b !='' && a.length < 50) {
-        perconalMovieDB.movies[a] = b;
-        console.log('done');
-      } else {
-        console.log('error!');
-        i--;
-      }
-    }
-  },
-  detectPersonalLevel: function() {
-    if (perconalMovieDB.count < 10) {
-      console.log('Просмотрено слишком мало фильмов');
-    } else if (perconalMovieDB.count >= 10 && perconalMovieDB.count < 30) {
-      console.log('Вы классический зритель');
-    } else if (perconalMovieDB.count >= 30) {
-      console.log('Вы киноман!');
-    } else {
-      console.log('Произошла ошибка!');
-    }
-  },
-  showMyDB: function() {
-    if (perconalMovieDB.privat != true) {
-      console.log(perconalMovieDB);
-    }
-  },
-  toggleVisibleDB: function() {
-    if (perconalMovieDB.privat) {
-      perconalMovieDB.privat = false;
-    } else {
-      perconalMovieDB.privat = true;
-    }
-  },
-  writeYourGenres:  function() {
-    for (let i = 1; i <= 3; i++) {
-      let genre = prompt(`Ваш любимый жанр под номером ${i}`).toLowerCase();
-
-      if (genre == '' || genre == null) {
-        console.log('Вы ввели некорректные данные или не ввели данные');
-        i--;
-      }else {
-        perconalMovieDB.genres[i - 1] = genre;
-        perconalMovieDB.genres.sort();
-      } 
-    } 
-    perconalMovieDB.genres.forEach((item, i) => {
-      console.log(`Любимый жанр ${i + 1} - это ${item}`);
-    });
-  }
+const movieDB = {
+  movies: [
+      "Логан",
+      "Лига справедливости",
+      "Ла-ла лэнд",
+      "Одержимость",
+      "Скотт Пилигрим против..."
+  ]
 };
 
+const promoAdv = document.querySelectorAll('.promo__adv img'),
+      promoBg = document.querySelector('.promo__bg'),
+      genre = promoBg.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
 
+  genre.textContent = 'Драма'; //2
+
+  promoBg.style.backgroundImage = 'url("/img/bg.jpg")';//3
+
+promoAdv.forEach(item => {
+  item.remove();
+}); //1
+
+movieList.innerHTML = '';//4
+
+movieDB.movies.sort(); //4 
+
+movieDB.movies.forEach((film, i) => {
+  movieList.innerHTML += `
+  <li class="promo__interactive-item">${i + 1}  ${film}
+      <div class="delete"></div>
+  </li>
+  `;
+});//5
--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 "use strict";
 
-// let x = 5;
-// alert(x++);
+// const box = document.getElementById('box');
+// const buttons = document.getElementsByTagName('button');
+// const circle = document.getElementsByClassName('circle');
+// const hearts = document.querySelectorAll('.heart');
+// const oneHeart = document.querySelector('.heart');
+// const wrapper = document.querySelector('.wrapper');
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+// box.style.marginLeft = '35px';
+// box.style.cssText = 'background-color: pink; width: 200px';
+// buttons[3].style.backgroundColor = 'green';
+// circle[1].style.backgroundColor = 'green';
 
-// [ ] + false - null + true  //Ответ NaN
+// for (let i = 0; i < hearts.length; i++) {
+//   hearts[i].style.backgroundColor = 'blue';
+// }
+// hearts.forEach(item => {
+//   item.style.backgroundColor = 'green';
+// });
 
-// let y = 1;
-// let x = y = 2;
-// alert(x);
-// console.log([] + 1 + 2);
-// // 2 && 1 && null && 0 && undefined ?
+// const div = document.createElement('div');
+// // const text = document.createTextNode('Тут был я');
+// div.classList.add('black');
+// // document.body.append(div);
+// wrapper.append(div);
+// wrapper.prepend(div);
+// hearts[0].before(div);
+// hearts[0].after(div);
+// circle[0].remove();
+// hearts[1].replaceWith(circle[1]);
 
-// console.log(2 && 1 && null && 0 && undefined);
-
-// console.log(!!(1 && 2 ));
-// console.log((1 && 2 ));
-alert( null || 2 && 3 || 4 ); 
-const a = [1, 2, 3]; 
-const b = [1, 2, 3]; 
-console.log(a == b);
+/*----Устаревшие методы но тоже встречаются в коде------ */
+// wrapper.appendChild(div);
+// wrapper.insertBefore(div, hearts[1]);
+// wrapper.removeChild(hearts[0]);  //Удаляет элемент
+// wrapper.replaceChild(circle[0], hearts[1]);
+// div.innerHTML = '<h1>Hello Miass</h1>';
+// div.textContent = 'RumRUM';
+// div.insertAdjacentHTML("beforeend", "<h3>Hi bob</h3>");
