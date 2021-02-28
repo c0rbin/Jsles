86e9ee2cfcca6cc6b957c181101b95c6a4d9c5e4
Dinis <example@gmail.com>
2021-02-28 11:44:30 +0500

    Добавил ссылку кнопку аниме
    И изменил цвет на красный

--- practic.js
+++ practic.js
@@ -13,8 +13,14 @@ const movieDB = {
 const promoAdv = document.querySelectorAll('.promo__adv img'),
       promoBg = document.querySelector('.promo__bg'),
       genre = promoBg.querySelector('.promo__genre'),
-      movieList = document.querySelector('.promo__interactive-list');
-
+      movieList = document.querySelector('.promo__interactive-list'),
+      formAdd = document.querySelector('.add'),
+      promoMenu = document.querySelector('.promo__menu-list ul'),
+      itemNav = document.createElement('li'),
+      promoItem = document.createElement('a'),
+      promoIntTitle = formAdd.querySelector('.promo__interactive-title');
+      
+promoItem.setAttribute('href','#');
   genre.textContent = 'Драма'; //2
 
   promoBg.style.backgroundImage = 'url("/img/bg.jpg")';//3
@@ -34,3 +40,12 @@ movieDB.movies.forEach((film, i) => {
   </li>
   `;
 });//5
+
+promoItem.classList = 'promo__menu-item';
+promoItem.textContent = 'Аниме';
+promoMenu.append(itemNav);
+
+
+itemNav.append(promoItem);
+
+promoIntTitle.style.color = 'red';
