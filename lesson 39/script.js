9b00371b88b33a5d0843234b456add5aaf0da5f1
Dinis <example@gmail.com>
2021-04-25 17:37:58 +0500

    Скрипты и время их выполнения. setTimeOut
    и setInterval

---
+++ lesson 39/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const btn = document.querySelector('.btn');
+let timerId,
+    i = 0;
+
+
+function myAnimation() {
+  const elem = document.querySelector('.box');
+
+  let pos = 0;
+
+  const id = setInterval(frame, 10);
+
+  function frame() {
+    if (pos == 300) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+    }
+  }
+}
+btn.addEventListener('click', myAnimation);
+
+// function logger() {
+//   if (i === 3) {
+//     clearInterval(timerId);
+//   }
+//   console.log('text');
+//   i++;
+// }
+
+// let id = setTimeout(function log() {
+//   console.log('Cap');
+//   id = setTimeout(log, 500);
+// }, 500);
