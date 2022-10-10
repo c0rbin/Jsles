602a7df32cfca1c3cb5aef8eae1a7cc5ae6eff2f
Dinis <example@gmail.com>
2022-10-10 08:21:47 +0500

    Themnes object destructirization

--- lesson/script.js
+++ lesson/script.js
@@ -6,6 +6,24 @@ const options = {
   height: 1024,
   colors: {
     border: 'black',
-    bg : 'red'
+    bg: 'red'
+  },
+  makeTest: function() {
+    console.log('Hi test');
   }
 };
+options.makeTest();
+console.log(Object.keys(options).length);
+
+let counter = 0;
+for (let key in options) {
+  if (typeof(options[key]) === 'object') {  
+    for (let i in options[key]) {
+      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+    }
+  } else {
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    counter++;
+  }
+}
+console.log(counter);
