d89e48bc73077b9ccee610ec9668844bbd55bd37
Dinis <example@gmail.com>
2023-01-07 01:11:53 +0500

    Создаем табы

--- lesson/script.js
+++ lesson/script.js
@@ -1,29 +1,10 @@
-"use strict";
+'use strict';
 
-const options = {
-  name: 'test',
-  width: 1024,
-  height: 1024,
-  colors: {
-    border: 'black',
-    bg: 'red'
-  },
-  makeTest: function() {
-    console.log('Hi test');
-  }
-};
-options.makeTest();
-console.log(Object.keys(options).length);
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
 
-let counter = 0;
-for (let key in options) {
-  if (typeof(options[key]) === 'object') {  
-    for (let i in options[key]) {
-      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-    }
-  } else {
-    console.log(`Свойство ${key} имеет значение ${options[key]}`);
-    counter++;
+wrapper.addEventListener('click', (e) => {
+  if (e.target && e.target.tagName == 'BUTTON') {
+    console.log('Hello');
   }
-}
-console.log(counter);
+});
