b09dc20805f00a46f0b9acd2e1d71bf7e16f7d53
Dinis <you@example.com>
2024-11-04 16:14:13 +0500

    Создать новое приложение на консоли

--- practic/script.js
+++ practic/script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+const numberOfFilms = +prompt('Cколько фильмов Вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+}
+
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
